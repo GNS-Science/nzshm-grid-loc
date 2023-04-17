ebde2913e52d0ef8fb1540e9a99874f7ad0b94bb
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-04-17 17:26:55 +1200

    updated verison, changelog, geopandas version;

--- nzshm_grid_loc/geography.py
+++ nzshm_grid_loc/geography.py
@@ -1,8 +1,9 @@
-
 import os
 import pathlib
 from enum import Enum
+
 from geopandas import GeoDataFrame
+
 from nzshm_grid_loc.io import load_polygon_file
 
 RESOURCES_FOLDER = pathlib.Path(pathlib.PurePath(os.path.realpath(__file__)).parent, 'resources')
@@ -14,7 +15,9 @@ class Regions(Enum):
     """
 
     # NZ = 'resources/nz-coastlines-and-islands-polygons-topo-150k.shp'
-    NZ_SMALL = str(RESOURCES_FOLDER) + '/small-nz.wkt.csv.zip'  # based on nz-coastlines-and-islands-polygons-topo-150k from LINZ
+    NZ_SMALL = (
+        str(RESOURCES_FOLDER) + '/small-nz.wkt.csv.zip'
+    )  # based on nz-coastlines-and-islands-polygons-topo-150k from LINZ
     WLG = str(RESOURCES_FOLDER) + '/wellington.geojson'  # Oakley just made this as a test
     BACKARC = str(RESOURCES_FOLDER) + '/backarc.geojson'
 
--- tests/test_nzshm_grid_loc.py
+++ tests/test_nzshm_grid_loc.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 """Tests for `nzshm_grid_loc` package."""
 from geopandas import GeoDataFrame
-import pytest
 
 
 def test_main_module_import():
     import nzshm_grid_loc as ngl
+
     assert ngl.__name__ == "nzshm_grid_loc"
+
 
 def test_top_level_module_members():
     import nzshm_grid_loc as ngl
-    regions_enum =  ngl.Regions
+
     assert ngl.Regions.NZ_SMALL is not None
 
 
 def test_load_region():
     import nzshm_grid_loc as ngl
+
     nzsmall = ngl.Regions.NZ_SMALL.load()
     assert isinstance(nzsmall, GeoDataFrame)
-
